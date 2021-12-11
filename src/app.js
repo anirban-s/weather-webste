5d0ea1cf5b218fe55f04f61d48da61bc3ca07e12
anirban-s <baban.chrome@gmail.com>
2021-12-11 21:04:08 +0530

    setup done for heroku

--- src/app.js
+++ src/app.js
@@ -84,6 +84,7 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Service is up on port 3000!");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Service is up on port " + port);
 });
